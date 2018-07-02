2d6e5a1b1f34a02b28662088000e76f66ca17c7b
Lbh <544289495@qq.com>
2018-07-02 16:43:31 +0800

    热更新

--- Code/起步/src/index.js
+++ Code/起步/src/index.js
@@ -1,20 +1,31 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './yeoman.png';
-import Data from './data.xml';
 import printMe from './print.js'
-function component(){
+
+function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
     btn.innerHTML = 'Click me and check the console';
     btn.onclick = printMe;
-    element.innerHTML = _.join(['Hello','webpack'],' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    // const myIcon = new Image();
+    // myIcon.src = Icon;
+    // element.appendChild(myIcon);
     element.appendChild(btn);
-    console.log(Data);
+    // console.log(Data);
     return element;
 }
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+console.log(module.hot);
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    })
+}
